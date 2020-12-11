444eb1a25694739db748a4f490dbdb7108aadece
aastyczynski <aa.styczynski@gmail.com>
2020-12-11 16:50:55 +0100

    Pass hash

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,32 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const {register_validation, login_validation} = require('../validation');
-
+const bcrypt = require('bcryptjs');
 
 
 
 router.post('/register',async (req, res) => {
 
-    // Validate data
+    //Validate data
     const {error} = register_validation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-    // User checking
+    //User checking
     const emailExist = await User.findOne({email: req.body.email});
     const usernameExist = await User.findOne({nazwa_uzytkownika: req.body.nazwa_uzytkownika});
     if(emailExist) return res.status(400).send("Email already exist");
     if(usernameExist) return res.status(400).send("Username already exist");
 
-    // Create a new user
+    //Hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassoword = await bcrypt.hash(req.body.haslo, salt);
+
+
+    //Create a new user
     const user = new User ({
         nazwa_uzytkownika: req.body.nazwa_uzytkownika,
         email: req.body.email,
-        haslo: req.body.haslo,
+        haslo: hashPassoword,
         imie: req.body.imie,
         nazwisko: req.body.nazwisko,
         pesel: req.body.pesel,
